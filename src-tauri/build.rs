26c8c2acc8f49d1cd1d96631ce32ae048b7ebbb5
Leastrio <jacob@leastr.io>
2023-11-28 01:50:15 -0700

    Make this app only compilable for windows

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-  tauri_build::build()
+  if cfg!(target_os = "windows") {
+    tauri_build::build()
+  } else {
+    println!("Error: This application can only be used and compiled for Windows.");
+    std::process::exit(1);
+  }
 }
